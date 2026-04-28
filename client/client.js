3ca8a1eda6cf03340fb008912ff6e9ce0b067d7e
iskvrw <qfjh44@durham.ac.uk>
2026-04-28 17:39:24 +0100

    added condition checking for login/logout

--- client/client.js
+++ client/client.js
@@ -1,60 +1,13 @@
-const fs = require("fs");
+if (localStorage.getItem("username") !== null) {
+    let register_button = document.getElementById("register_button");
+    register_button.innerHTML = "";
 
-async function Register() {
-    if (document.getElementById("password-field-one") === document.getElementById("password-field-two")) {
-        try {
-            const parameters = new URLSearchParams();
-            parameters.append("email", document.getElementById("email"));
-            parameters.append("password", document.getElementById("password-field-one"));
-            parameters.append("username", document.getElementById("username"));
-            parameters.append("first_name", document.getElementById("first_name"));
-            const url = "http://127.0.0.1:5500/client/register.html/register_data?" + parameters.toString();
-
-            const response = await fetch(url);
-            username = response.username;
-            first_name = response.first_name;
-            
-            /*
-            SAVE USERNAME AND FIRST_NAME TO LOCALSTORAGE
-            Don't forget to add it upon LOGIN
-            and
-            Delete upon LOGOUT
-            */
-
-
-        }
-        catch {
-            document.getElementById("error").innerHTML = "There already exists an account with this email. Please log in.";
-            Alert("There has been an error. Please try again.")
-        }
-    }
+    let login_button = document.getElementById("login_button");
+    login_button.innerHTML = "Logout";
 }
-
-async function Login() {
-    const parameters = new URLSearchParams();
-    parameters.append("email", document.getElementById("email"));
-    parameters.append("password", document.getElementById("password"));
-    const url = "http://127.0.0.1:5500/client/register.html/login_data?" + parameters.toString();
-
-    const response = await fetch(url);
-    username = response.username;
-    first_name = response.first_name;
-    
+else {
+    let register_button = document.getElementById("register_button");
+    register_button.innerHTML = "Register";
+    let login_button = document.getElementById("login_button");
+    login_button.innerHTML = "Login";
 }
-
-async function Logout() {
-    username = "";
-    first_name = "";
-    const url = "http://127.0.0.1:5500/client/register.html/logout";
-
-    const response = await fetch(url);
-}
-
-let registerSubmit = document.getElementById("registerSubmit");
-registerSubmit.addEventListener("click", Register);
-
-let loginSubmit = document.getElementById("loginSubmit");
-loginSubmit.addEventListener("click", Login);
-
-let logoutButton = document.getElementById("logoutButton");
-logoutButton.addEventListener("click", Logout);
